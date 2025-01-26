25d5b0ddeb079d3d98536a71fb19ce4348747ebc
Prarthana Rajapurohit <90079998+prar1907@users.noreply.github.com>
2025-01-26 11:55:42 -0800

    Add files via upload

---
+++ final_1.ino
@@ -0,0 +1,211 @@
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-light-sensor
+ */
+
+#include "DHT.h"
+#include <Arduino.h>
+#include <Wire.h>
+
+#include <BMx280TwoWire.h>
+
+#define I2C_ADDRESS 0x76
+
+//create a BMx280I2C object using the I2C interface with I2C Address 0x76
+BMx280TwoWire bmx280(&Wire, I2C_ADDRESS);
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 3 (on the right) of the sensor to GROUND (if your sensor has 3 pins)
+// Connect pin 4 (on the right) of the sensor to GROUND and leave the pin 3 EMPTY (if your sensor has 4 pins)
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
+int measurePin = A5;
+int ledPower = 12;
+
+unsigned int samplingTime = 280;
+unsigned int deltaTime = 40;
+unsigned int sleepTime = 9680;
+
+float voMeasured = 0;
+float calcVoltage = 0;
+float dustDensity = 0;
+
+void setup() {
+
+  pinMode(8,INPUT);
+pinMode(12,OUTPUT);
+pinMode(ledPower,OUTPUT);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+  while (!Serial);
+
+	Wire.begin();
+
+	//begin() checks the Interface, reads the sensor ID (to differentiate between BMP280 and BME280)
+	//and reads compensation parameters.
+	if (!bmx280.begin())
+	{
+		Serial.println("begin() failed. check your BMx280 Interface and I2C Address.");
+		while (1);
+	}
+
+	if (bmx280.isBME280())
+		Serial.println("sensor is a BME280");
+	else
+		Serial.println("sensor is a BMP280");
+
+	//reset sensor to default parameters.
+	bmx280.resetToDefaults();
+
+	//by default sensing is disabled and must be enabled by setting a non-zero
+	//oversampling setting.
+	//set an oversampling setting for pressure and temperature measurements. 
+	bmx280.writeOversamplingPressure(BMx280MI::OSRS_P_x16);
+	bmx280.writeOversamplingTemperature(BMx280MI::OSRS_T_x16);
+
+	//if sensor is a BME280, set an oversampling setting for humidity measurements.
+	if (bmx280.isBME280())
+		bmx280.writeOversamplingHumidity(BMx280MI::OSRS_H_x16);
+}
+
+void loop() {
+  // reads the input on analog pin A0 (value between 0 and 1023)
+  int analogValue = analogRead(A0);
+
+  Serial.print("Analog reading: ");
+  Serial.print(analogValue);   // the raw analog reading
+
+  // We'll have a few threshholds, qualitatively determined
+  if (analogValue < 150) {
+    Serial.println(" - very bright");
+  } else if (analogValue < 500) {
+    Serial.println(" - bright");
+  } else if (analogValue < 800) {
+    Serial.println(" - Light");
+  } else if (analogValue < 1600) {
+    Serial.println(" - dim");
+  } else {
+    Serial.println(" - dark");
+  }
+
+  delay(500);
+
+  if(digitalRead(8)==LOW){
+  Serial.println(F("No obstacle detected"));
+  }
+  else{
+  Serial.println(F("IR interrupted"));
+}
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+
+  delay(1000);
+  if (!bmx280.measure())
+	{
+		Serial.println("could not start measurement, is a measurement already running?");
+		return;
+	}
+
+	//wait for the measurement to finish. 
+	do
+	{
+		delay(100);
+	} while (!bmx280.hasValue());
+
+	//important: measurement data is read from the sensor in function hasValue() only. 
+	//make sure to call get*() functions only after hasValue() has returned true. 
+	Serial.print("Pressure: "); Serial.println(bmx280.getPressure());
+	Serial.print("Pressure (64 bit): "); Serial.println(bmx280.getPressure64());
+	Serial.print("Temperature: "); Serial.println(bmx280.getTemperature());
+
+	if (bmx280.isBME280())
+	{
+		Serial.print("Humidity: "); 
+		Serial.println(bmx280.getHumidity());
+	}
+
+  delay(1000);
+  digitalWrite(ledPower,LOW);
+  delayMicroseconds(samplingTime);
+
+  voMeasured = analogRead(measurePin);
+
+  delayMicroseconds(deltaTime);
+  digitalWrite(ledPower,HIGH);
+  delayMicroseconds(sleepTime);
+
+  calcVoltage = voMeasured*(5.0/1024);
+  dustDensity = 0.17*calcVoltage-0.1;
+
+  if ( dustDensity < 0)
+  {
+    dustDensity = 0.00;
+  }
+
+  Serial.println("Raw Signal Value (0-1023):");
+  Serial.println(voMeasured);
+
+  Serial.println("Voltage:");
+  Serial.println(calcVoltage);
+
+  Serial.println("Dust Density:");
+  Serial.println(dustDensity);
+
+  delay(1000);
+}
+
